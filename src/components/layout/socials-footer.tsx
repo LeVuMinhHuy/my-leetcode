2f9035e34100ea9d0a8008f6084920a14a51b91e
Ted Vu <ted@fwd.com>
2025-02-13 16:02:16 +0700

    add socials

--- src/components/layout/socials-footer.tsx
+++ src/components/layout/socials-footer.tsx
@@ -1,6 +1,6 @@
 import { ModeToggle } from '@/components/theme/toggle-mode';
 import { Button } from '@/components/ui/button';
-import { Github, Globe, Twitter } from 'lucide-react';
+import { Github, Globe, Instagram } from 'lucide-react';
 import { Link } from '../custom/link';
 
 export function SocialsFooter() {
@@ -9,17 +9,17 @@ export function SocialsFooter() {
 			<div className='flex justify-center items-center gap-2 p-1'>
 				<ModeToggle className='[&>svg]:h-4 [&>svg]:w-4' />
 				<Button variant='ghost' size='sm' className='w-9 px-0' asChild>
-					<Link href=''>
+					<Link href='https://github.com/LeVuMinhHuy/my-leetcode'>
 						<Github className='h-4 w-4' />
 					</Link>
 				</Button>
 				<Button variant='ghost' size='sm' className='w-9 px-0' asChild>
-					<Link href=''>
-						<Twitter className='h-4 w-4' />
+					<Link href='https://www.instagram.com/mh.hmmm/'>
+						<Instagram className='h-4 w-4' />
 					</Link>
 				</Button>
 				<Button variant='ghost' size='sm' className='w-9 px-0' asChild>
-					<Link href=''>
+					<Link href='https://tedvu.me'>
 						<Globe className='h-4 w-4' />
 					</Link>
 				</Button>

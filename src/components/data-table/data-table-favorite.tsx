1297e26b18b71af7aafbf3905c0db8bbf65595d4
Phuc Nguyen <nguyenvuhoangphuc11@gmail.com>
2025-02-16 01:33:34 +0700

    add state for favorite cell

--- src/components/data-table/data-table-favorite.tsx
+++ src/components/data-table/data-table-favorite.tsx
@@ -1,25 +1,34 @@
-'use client';
+"use client";
 
-import { updateProblem } from '@/services/updateProblem';
-import { Star } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
+import { updateProblem } from "@/services/updateProblem";
+import { Star } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 
-export function DataTableFavorite({ id, favorite }: { id: number; favorite: boolean }) {
-	const router = useRouter();
+export function DataTableFavorite({
+  id,
+  favorite,
+}: {
+  id: number;
+  favorite: boolean;
+}) {
+  const router = useRouter();
+  const [fav, setFav] = useState(favorite);
 
-	const onChangeFavorite = useCallback(async () => {
-		await updateProblem(id, { favorite: !favorite }, false);
-		router.refresh();
-	}, [favorite]);
+  const onChangeFavorite = useCallback(async () => {
+    const res = await updateProblem(id, { favorite: !fav }, false);
+    if (res) {
+      setFav(!fav);
+    }
+  }, [fav]);
 
-	return (
-		<Star
-			size={16}
-			className={`ml-6 cursor-pointer transition-colors ${
-				favorite ? 'fill-yellow-500 text-yellow-500' : 'fill-none text-gray-400'
-			}`}
-			onClick={onChangeFavorite}
-		/>
-	);
+  return (
+    <Star
+      size={16}
+      className={`ml-6 cursor-pointer transition-colors ${
+        fav ? "fill-yellow-500 text-yellow-500" : "fill-none text-gray-400"
+      }`}
+      onClick={onChangeFavorite}
+    />
+  );
 }

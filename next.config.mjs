601ee5ca37d4e24ee21cb6ce5e2a859cf60db1b7
Ted Vu <levuminhhuy99@gmail.com>
2025-02-19 17:22:32 +0700

    no need mongoose

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,22 @@ const nextConfig = {
 	experimental: {
 		// REMINDER: new react compiler to memoize the components
 		// https://react.dev/learn/react-compiler
-		reactCompiler: true
+		reactCompiler: true,
 	},
-  async redirects() {
-    return [
-      {
-				source: "/i",
-				destination: "/infinite",
-				permanent: true
-			},
+	reactStrictMode: true,
+	async headers() {
+		return [
 			{
-				source: "/vercel",
-				destination: "/infinite",
-				permanent: true
-			}
-		]
-	}
+				source: '/',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 's-maxage=1, stale-while-revalidate=59',
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
